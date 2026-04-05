7224bd86f8ca0ee1f54fff1e1162b3afef6144a2
Muhammad Muneeb <mmuneeb840@gmail.com>
2026-04-05 19:11:18 +0500

    Fix blank screen: remove CRA index.html, add static export + netlify config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   transpilePackages: ['antd', 'rc-util', 'rc-pagination', 'rc-picker', 'rc-input'],
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
